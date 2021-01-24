6b45da989b29b13dcc5cf2e16756eae6f878d95b
jxsl13 <jxsl13@gmail.com>
2021-01-24 20:01:21 +0100

    update events with Marshal/Unmarshal functions

--- events/base.go
+++ events/base.go
@@ -1,5 +1,7 @@
 package events
 
+import "encoding/json"
+
 // BaseEvent contains the common fields that all events have.
 type BaseEvent struct {
 	// Type of the event
@@ -12,4 +14,12 @@ type BaseEvent struct {
 
 	// When was the event created
 	Timestamp string `json:"timestamp,omitempty"`
+}
+
+// Unmarshal can be used on any event that is a composition of the BaseEvent
+// in order to distinguish between different event types that are
+// correctly retrieved by this function.
+// Depending on the type, one can properly unmarshal the corresponding event type
+func (be *BaseEvent) Unmarshal(payload string) error {
+	return json.Unmarshal([]byte(payload), be)
 }
--- events/player_join.go
+++ events/player_join.go
@@ -1,13 +1,44 @@
 package events
 
+import "encoding/json"
+
 // PlayerJoinEvent is created when a player joins some server.
 type PlayerJoinEvent struct {
 	BaseEvent
-	ID      int    `json:"id,omitempty"`
 	Name    string `json:"name,omitempty"`
 	Clan    string `json:"clan,omitempty"`
-	Country int    `json:"country,omitempty"`
 	IP      string `json:"ip,omitempty"`
 	Port    int    `json:"port,omitempty"`
+	ID      int    `json:"id,omitempty"`
+	Country int    `json:"country,omitempty"`
 	Version int    `json:"version,omitempty"`
+}
+
+// NewEventPlayerJoin creates a new PlayerJoinEvent
+func NewEventPlayerJoin(source, timestamp, name, clan, ip string, port, id, country, version int) PlayerJoinEvent {
+	return PlayerJoinEvent{
+		BaseEvent: BaseEvent{
+			Type:      TypePlayerJoin,
+			Source:    source,
+			Timestamp: timestamp,
+		},
+		Name:    name,
+		Clan:    clan,
+		IP:      ip,
+		Port:    port,
+		ID:      id,
+		Country: country,
+		Version: version,
+	}
+}
+
+// Marshal creates a json string from the current struct
+func (pje *PlayerJoinEvent) Marshal() string {
+	b, _ := json.Marshal(pje)
+	return string(b)
+}
+
+// Unmarshal fills the current struct with the unmarshalled values
+func (pje *PlayerJoinEvent) Unmarshal(payload string) error {
+	return json.Unmarshal([]byte(payload), pje)
 }
