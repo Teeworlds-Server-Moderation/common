a17701594db37d4a5adad64766494285ee2c313d
jxsl13 <jxsl13@gmail.com>
2021-01-27 23:04:34 +0100

    extend dto.Player with potentially used fields

--- dto/player.go
+++ dto/player.go
@@ -10,4 +10,15 @@ type Player struct {
 	ID      int    `json:"id,omitempty"`
 	Country int    `json:"country,omitempty"`
 	Version int    `json:"version,omitempty"`
+	Kills   int    `json:"kills,omitempty"`
+	Deaths  int    `json:"deaths,omitempty"`
+	Score   int    `json:"score,omitempty"`
+	Wins    int    `json:"wins,omitempty"`
 }
+
+// PlayersSortByID sort a list of player by their ID
+type PlayersSortByID []Player
+
+func (a PlayersSortByID) Len() int           { return len(a) }
+func (a PlayersSortByID) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a PlayersSortByID) Less(i, j int) bool { return a[i].ID < a[j].ID }
