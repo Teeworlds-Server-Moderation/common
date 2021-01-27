49fdc9b4686c708bd20438297250c114feca728c
jxsl13 <jxsl13@gmail.com>
2021-01-27 23:30:18 +0100

    add custom topic that is used to receive broadcasted execution commands

---
+++ mqtt/topics.go
@@ -0,0 +1,7 @@
+package mqtt
+
+const (
+	// TopicBroadcast Every server monitor subscribes to this
+	// topic in order to react to possible broadcasted command execution events
+	TopicBroadcast = "BROADCAST"
+)
