dff21b58a0446e33124d31204bee2c4d22618977
jxsl13 <jxsl13@gmail.com>
2021-01-28 22:19:01 +0100

    add .env parsing package for convenience

---
+++ env/env.go
@@ -0,0 +1,68 @@
+package env
+
+import (
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+	configo "github.com/jxsl13/simple-configo"
+)
+
+// Read reads .env files that are passed to this function and returns a map of key values
+// .env files have the same syntax as environment variables KEY=VALUE
+func Read(filenames ...string) (map[string]string, error) {
+	return godotenv.Read(filenames...)
+}
+
+// GetEnv returns a map of OS environment variables
+func GetEnv() map[string]string {
+	pairs := os.Environ()
+	env := make(map[string]string, len(pairs))
+	for _, pair := range pairs {
+		keyPairs := strings.SplitN(pair, "=", 2)
+		if len(keyPairs) == 0 {
+			continue
+		} else if len(keyPairs) == 1 {
+			env[keyPairs[0]] = ""
+		} else {
+			env[keyPairs[0]] = keyPairs[1]
+		}
+	}
+	return env
+}
+
+// Get first tries to read the passed files and then
+// overrides the .env file parameters with the environment variables
+// in memory.
+// this is useful in order to have a base configuration that is within a file
+// that can contain like a common password for every econ, but must override its
+// econ address (IP:Port) with an environment variable.
+// The passed filenames can be omitted, but may be used to pass a list of files.
+func Get(filenames ...string) map[string]string {
+	fileEnv, err := Read(filenames...)
+	if err != nil {
+		// Could not fetch those files, thus simply
+		// returning environment varibles
+		return GetEnv()
+	}
+
+	env := GetEnv()
+	for key, value := range env {
+		fileEnv[key] = value
+	}
+	return fileEnv
+}
+
+// Parse uses my simple-configo package that allows to easily parse
+// environment maps and do proper value conversions as well as setting your
+// configuration's struct fields after parsing the values.
+// All you do is define a Configuration struct, add any of the common struct
+// field values, implement the Options() configo.Options function and you are ready to parse
+// any arbitrary environment format like string, regex match, integer, bool, time ranges, etc.
+// filenames is an optional parameter, if it is left empty, the underlying library will try
+// to open the .env file in the same directory first and then parse those values
+// afterwards the environment variables override any value from that file.
+func Parse(cfg configo.Config, filenames ...string) error {
+	env := Get(filenames...)
+	return configo.Parse(cfg, env)
+}
--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.15
 require (
 	github.com/eclipse/paho.mqtt.golang v1.3.1
 	github.com/google/uuid v1.2.0
+	github.com/joho/godotenv v1.3.0
+	github.com/jxsl13/simple-configo v1.2.4
 )
