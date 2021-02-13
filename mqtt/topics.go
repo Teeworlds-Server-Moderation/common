341c59b08dd451c0a354bd0eafd19e59815d32c7
jxsl13 <jxsl13@gmail.com>
2021-02-13 11:44:41 +0100

    create dedicated topics package

--- mqtt/topics.go
+++
@@ -1,7 +0,0 @@
-package mqtt
-
-const (
-	// TopicBroadcast Every server monitor subscribes to this
-	// topic in order to react to possible broadcasted command execution events
-	TopicBroadcast = "BROADCAST"
-)
---
+++ topics/topics.go
@@ -0,0 +1,7 @@
+package topics
+
+const (
+	// Broadcast Every server monitor subscribes to this
+	// topic in order to react to possible broadcasted command execution events
+	Broadcast = "BROADCAST"
+)
