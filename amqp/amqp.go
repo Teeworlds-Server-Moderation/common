2a26155c635465594355332f32e9ba20dbeb7fbc
jxsl13 <jxsl13@gmail.com>
2021-02-15 21:27:30 +0100

    remove unused function

--- amqp/amqp.go
+++ amqp/amqp.go
@@ -1,24 +1,10 @@
 package amqp
 
-import "github.com/streadway/amqp"
+import (
+	"time"
+)
 
-// common queue creation that is used for publisher and subscriber
-// if this function is different for any of the two, you may get problems with the protocol stack of amqp,
-// as you will get an error when declaring a que differently if a queue already exists.
-// Idempotence is only guaranteed if created and to be created queue configurations match.
-func createQueuesIfNotExists(channel *amqp.Channel, queues ...string) error {
-
-	for _, queue := range queues {
-		// declare queue if unknown
-		_, err := channel.QueueDeclare(
-			queue, // name
-			true,  // durable
-			true,  // delete when unused
-			false, // exclusive
-			false, // no-wait
-			nil,   // arguments
-		)
-		return err
-	}
-	return nil
-}
+var (
+	// ReconnectTimeout is the default value used to reconnect until the subscribers and publishers return an error
+	ReconnectTimeout = time.Minute
+)
