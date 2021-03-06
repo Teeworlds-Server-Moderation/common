40e01bafe42387a477e14ccf9b9171c19ca8dd9f
jxsl13 <jxsl13@gmail.com>
2021-03-06 12:53:18 +0100

    add accesslevel for player objects

--- dto/player.go
+++ dto/player.go
@@ -5,17 +5,18 @@ import "encoding/json"
 // Player is the object that represents a player
 //  with all of their information.
 type Player struct {
-	Name    string `json:"name,omitempty"`
-	Clan    string `json:"clan,omitempty"`
-	IP      string `json:"ip,omitempty"`
-	Port    int    `json:"port,omitempty"`
-	ID      int    `json:"id,omitempty"`
-	Country int    `json:"country,omitempty"`
-	Version int    `json:"version,omitempty"`
-	Kills   int    `json:"kills,omitempty"`
-	Deaths  int    `json:"deaths,omitempty"`
-	Score   int    `json:"score,omitempty"`
-	Wins    int    `json:"wins,omitempty"`
+	Name        string `json:"name,omitempty"`
+	Clan        string `json:"clan,omitempty"`
+	IP          string `json:"ip,omitempty"`
+	Port        int    `json:"port,omitempty"`
+	ID          int    `json:"id,omitempty"`
+	Country     int    `json:"country,omitempty"`
+	Version     int    `json:"version,omitempty"`
+	Kills       int    `json:"kills,omitempty"`
+	Deaths      int    `json:"deaths,omitempty"`
+	Score       int    `json:"score,omitempty"`
+	Wins        int    `json:"wins,omitempty"`
+	AccessLevel string `json:"access_level,omitempty"`
 }
 
 // Marshal creates a json string from the current struct
