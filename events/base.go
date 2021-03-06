21a8272c9caee1fff6f0ddf3f92546381aa264c5
jxsl13 <jxsl13@gmail.com>
2021-03-06 12:54:49 +0100

    allow creation of automatic timestamped BaseEvent

--- events/base.go
+++ events/base.go
@@ -24,3 +24,17 @@ type BaseEvent struct {
 func (be *BaseEvent) Unmarshal(payload string) error {
 	return json.Unmarshal([]byte(payload), be)
 }
+
+// SetEventSource allows to set the event's source, e.g. which server/econ connection created this event
+func (be *BaseEvent) SetEventSource(eventSource string) {
+	be.EventSource = eventSource
+}
+
+// NewBaseEventTimestamped creates a new BaseEvent that is timestamped with time.Now()
+// and has the passed eventType as type parameter. The EventSource is left empty.
+func NewBaseEventTimestamped(eventType string) BaseEvent {
+	return BaseEvent{
+		Type:      eventType,
+		Timestamp: FormatedTimestamp(),
+	}
+}
