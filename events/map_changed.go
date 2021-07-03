dbdbcec092e8209b6181b54ddedf78d2db7f07dc
John Behm <jxsl13@gmail.com>
2021-07-03 19:16:48 +0200

    MapChaned -> MapChanged

--- events/map_changed.go
+++ events/map_changed.go
@@ -4,27 +4,27 @@ import (
 	"encoding/json"
 )
 
-// MapChanedEvent is created when the map is actually changed after an option vote passes
-type MapChanedEvent struct {
+// MapChangedEvent is created when the map is actually changed after an option vote passes
+type MapChangedEvent struct {
 	BaseEvent
 	OldMap string `json:"old_map,omitempty"`
 	NewMap string `json:"new_map,omitempty"`
 }
 
 // Marshal creates a json string from the current struct
-func (mce *MapChanedEvent) Marshal() string {
+func (mce *MapChangedEvent) Marshal() string {
 	b, _ := json.Marshal(mce)
 	return string(b)
 }
 
 // Unmarshal fills the current struct with the unmarshalled values
-func (mce *MapChanedEvent) Unmarshal(payload string) error {
+func (mce *MapChangedEvent) Unmarshal(payload string) error {
 	return json.Unmarshal([]byte(payload), mce)
 }
 
-// NewMapChanedEvent create an empty event with a proper event type
-func NewMapChanedEvent() MapChanedEvent {
-	event := MapChanedEvent{}
+// NewMapChangedEvent create an empty event with a proper event type
+func NewMapChangedEvent() MapChangedEvent {
+	event := MapChangedEvent{}
 	event.BaseEvent = NewBaseEventTimestamped(TypeMapChanged)
 	return event
 }
